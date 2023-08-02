d3ed27b503f3275b1252e6a2462d49f22baa1497
EngEmadHany <emadhany2007@gmail.com>
2023-08-03 00:07:30 +0400

    Initial Commit

---
+++ SwiftUITinderTrip/SwiftUITinderTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITinderTripApp.swift
+//  SwiftUITinderTrip
+//
+//  Created by Emad Isaac on 30/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITinderTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
